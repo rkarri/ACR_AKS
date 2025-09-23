ebc3b806fb29303a52fa9dbcd4247e7618be4db6
raviK <ravi-karri@hotmail.com>
2025-09-23 16:43:47 -0500

    added Dockerfile and other changes

---
+++ ACR_AKS/WebApplication1/tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace RaviK.CICD.API.tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
